d6dd345f259a41d6b84114ce179960464d8bee8e
daikon128 <ndaichi06@gmail.com>
2021-01-30 14:05:28 +0900

    :sparkles:カスタムボタン作成

---
+++ src/components/CustomButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <button>{{ prefix }} {{ label }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CustomButton",
+  props: {
+    name: String
+  },
+  data() {
+    return {
+      text: "カスタムボタン:",
+    };
+  },
+}
+</script>
