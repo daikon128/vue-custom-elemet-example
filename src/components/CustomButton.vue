76e0abe17cd263540c4b906dcd8789bdad84d52a
daikon128 <ndaichi06@gmail.com>
2021-01-30 14:15:53 +0900

    :sparkles: コンパイルの設定

--- src/components/CustomButton.vue
+++ src/components/CustomButton.vue
@@ -12,7 +12,7 @@ export default {
   },
   data() {
     return {
-      text: "カスタムボタン:",
+      prefix: "カスタムボタン:",
     };
   },
 }
--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
 import Vue from 'vue'
+import vueCustomElement from "vue-custom-element";
+import CustomButton from "./components/CustomButton.vue";
 
 Vue.config.productionTip = false
 
+Vue.config.ignoredElements = ["custom-button"];
+Vue.use(vueCustomElement);
+Vue.customElement("custom-button", CustomButton, {
+  shadow: true,
+  shadowCss: `
+      button {
+        cursor: pointer;
+        background: wheat;
+        height: 30px;
+        width: 120px;
+      }
+    `,
+})
